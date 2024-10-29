c737e82ba4d7c0faeac7e6fd909cb98dcf5bee93
dohun0310 <dohun0310@naver.com>
2024-10-29 14:30:10 +0900

    add necessary get requests

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,17 @@ const app = express();
 const port = 6310;
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+  res.send("If you see this message, server is very healthy.");
 });
+
+app.get("/post", (req: Request, res: Response) => {
+  res.send("This is a post request.");
+});
+
+app.get("/categories", (req: Request, res: Response) => {
+  res.send("This is a category request.");
+});
+
+app.listen(port, () => {
+  console.log(`Your app is running at http://localhost:${port}`)
+})
